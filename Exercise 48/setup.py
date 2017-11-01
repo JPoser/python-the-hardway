d1b2fbc74b288ac78bc61d395f3ee33ede4466b3
Joseph Poser <jposer@a5134.local>
2017-11-01 11:17:04 +0000

    started exercise 48

---
+++ Exercise 48/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config = {
+	'description': 'ex48 course work',
+	'author': 'JPoser',
+	'url': 'https://github.com/JPoser/python-the-hardway',
+	'download_url': 'Where to download it',
+	'author_email': 'My email.',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['ex48'],
+	'scripts': [],
+	'name': 'ex48-lexicon'
+}
+
+setup(**config)
